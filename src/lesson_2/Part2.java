dbb349beaefe8465569d8f842f133f6b6b8527d1
vucic <vucic94@yahoo.com>
2025-02-07 19:47:15 +0100

    Lesson 2

---
+++ src/lesson_2/Part2.java
@@ -0,0 +1,17 @@
+package lesson_2;
+
+public class Part2 {
+    public static void main(String[] args) {
+        int a = 50;
+        int b = 20;
+        String message = "Hi";
+        System.out.println(message + " " + (a + b));
+        System.out.println(a + b + message);
+
+        String name = "MiLan!@#";
+        System.out.println(name.length());
+        String upper = name.toUpperCase();
+        System.out.println(upper);
+        System.out.println(name.toLowerCase());
+    }
+}
