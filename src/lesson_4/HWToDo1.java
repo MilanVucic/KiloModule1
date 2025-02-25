24a998b527e5693049ea290c654da13958c1eba4
vucic <vucic94@yahoo.com>
2025-02-25 19:28:59 +0100

    Lesson 5

--- src/lesson_4/HWToDo1.java
+++ src/lesson_4/HWToDo1.java
@@ -5,7 +5,7 @@ public class HWToDo1 {
     // How many days will it take to reach a 1000 if we start with 5 trees?
 
     // 2. Ask the user for input. Make sure it's positive. Ask for a divider (also positive)
-    // Calculate the sum of all numbers from 1-number that are divisible with divider
+    // Calculate the sum of all numbers from 1 to that number that are divisible with divider
     // number = 10, divider = 3
     // 3 + 6 + 9 = 18
     // number = 20, divider = 9
@@ -15,6 +15,7 @@ public class HWToDo1 {
     // Factorial of 5 => 5 * 4 * 3 * 2 * 1
 
     // 4. Write a program that generates a random number and asks the user to guess what the number is.
+    // Additionally, you can add lives, counting how many attempts it took to solve etc.
     public static void main(String[] args) {
 
     }
---
+++ src/lesson_5/Prime.java
@@ -0,0 +1,20 @@
+package lesson_5;
+
+public class Prime {
+    // Write a program to check whether a number is prime or not.
+    public static void main(String[] args) {
+        int input = 113; // 59/2 = 29
+        // 1003 -> No -> ....
+        // 113 -> root 11 * 11             11 * 11
+        // if we had a divider AFTER root, 19 * 6 = original
+
+        boolean prime = true;
+        for (int divider = 2; divider <= Math.sqrt(input); divider++) {
+            if (input % divider == 0) {
+                prime = false;
+                break;
+            }
+        }
+        System.out.println(prime);
+    }
+}
