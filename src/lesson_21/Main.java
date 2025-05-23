90b337ded4243e9a63905233e0691dc2014a8371
vucic <vucic94@yahoo.com>
2025-05-23 19:39:31 +0200

    Lesson 22

--- src/lesson_21/Main.java
+++ src/lesson_21/Main.java
@@ -1,6 +1,8 @@
 package lesson_21;
 
-import java.util.Arrays;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
 
 public class Main {
     // Additional HW: Make autocorrecter
@@ -13,15 +15,31 @@ public class Main {
     // renam -> rename, ream
     // read -> read, ready, red
 
+    // sitting -> si, it, tt, ti, in, ng
+    // sittong
+
+    // Levenstein distance
+    // sitting kitten -> 3
+    // sitting -> kitting -> kittin -> kitten
+
     public static void main(String[] args) {
+        try (CloseableEx c = new CloseableEx()) {
+            System.out.println("Doing stuff");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void triangles() {
         try {
             createTriangle();
         } catch (TriangleSideException e) {
-            System.out.println(Arrays.toString(e.getStackTrace()));
+            System.out.println(e.getMessage());
+            e.printStackTrace();
         }
     }
 
     private static void createTriangle() {
-        Triangle tr = new Triangle(1,2,3);
+        Triangle tr = new Triangle(1, 2, 3);
     }
 }
---
+++ src/lesson_22/Shape.java
@@ -0,0 +1,17 @@
+package lesson_22;
+
+public abstract class Shape {
+    private String color;
+    private int id;
+
+    public Shape(int id) {
+        this.id = id;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public abstract double getArea();
+    public abstract double getCircumference();
+}
