ac66473dfcdd8d904ec5a0ea79c18f630a1dc20b
vucic <vucic94@yahoo.com>
2025-05-27 19:25:48 +0200

    Lesson 23

---
+++ src/lesson_23/PlayerScore.java
@@ -0,0 +1,27 @@
+package lesson_23;
+
+public class PlayerScore implements Comparable<PlayerScore>{
+    private String name;
+    private int score;
+
+    public PlayerScore(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public static PlayerScore parseFromString(String line) {
+        String[] parts = line.split(" ");
+        // "Ana Maria-500"
+        return new PlayerScore(parts[0], Integer.parseInt(parts[1]));
+    }
+
+    @Override
+    public String toString() {
+        return name + " " + score;
+    }
+
+    @Override
+    public int compareTo(PlayerScore o) {
+        return o.score - this.score;
+    }
+}
